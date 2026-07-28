b1df22786ee7908b18908e1a0ef1505448970c40
angelicamazzei000-creator <angelica.mazzei000@gmail.com>
2026-07-29 00:27:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Incolla qui la configurazione della tua app Web Firebase.
 // Firebase Console → Impostazioni progetto → Le tue app → Web app.
-window.FIREBASE_CONFIG = { const app = initializeApp(firebaseConfig)
+window.FIREBASE_CONFIG = { const app = initializeApp(firebaseConfig)}
   apiKey: "INCOLLA_API_KEY",
   authDomain: "INCOLLA_PROJECT_ID.firebaseapp.com",
   databaseURL: "https://INCOLLA_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
