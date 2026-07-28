d3c3a801d25a00125f33ffb3430749f7be1a7316
angelicamazzei000-creator <angelica.mazzei000@gmail.com>
2026-07-29 00:32:27 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,16 @@
 // Incolla qui la configurazione della tua app Web Firebase.
 // Firebase Console → Impostazioni progetto → Le tue app → Web app.
-window.FIREBASE_CONFIG = { const app = initializeApp(firebaseConfig)}
+window.FIREBASE_CONFIG = { const firebaseConfig = {
+  apiKey: "AIzaSyAclh1hgV-WDe8KEvesU7LVqZkIn61upEc",
+  authDomain: "cartenerissime-b26df.firebaseapp.com",
+  databaseURL: "https://cartenerissime-b26df-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cartenerissime-b26df",
+  storageBucket: "cartenerissime-b26df.firebasestorage.app",
+  messagingSenderId: "818664229157",
+  appId: "1:818664229157:web:29d9dcc58cb0cced4bb2cc",
+  measurementId: "G-VM69LPKQ36"
+};
+}
   apiKey: "INCOLLA_API_KEY",
   authDomain: "INCOLLA_PROJECT_ID.firebaseapp.com",
   databaseURL: "https://INCOLLA_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
